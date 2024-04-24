66f5b61215d74c4620a42fa02558e081fc3b62de
Santanu Aich <rccsantanu@gmail.com>
2024-04-24 15:31:22 +0530

    jenkinsfile

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get("/users", async (req, res) => {
   try {
     const data = await connection.promise().query(`SELECT *  from user;`);
     res.status(200).json({
-      message: "Data retrieved successfully 4",
+      message: "Data retrieved successfully sayan",
       users: data[0],
     });
   } catch (err) {
