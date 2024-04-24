de740c5d0ce259b0fcfd0883d0c0a4ad929d69cf
Santanu Aich <rccsantanu@gmail.com>
2024-04-24 18:14:19 +0530

    jenkinsfile

--- index.js
+++ index.js
@@ -15,94 +15,10 @@ const connection = mysql.createPool({
 
 // post request
 
-app.post("/users", async (req, res) => {
-  try {
-    const { name, address, country } = req.body;
-    const [{ insertId }] = await connection.promise().query(
-      `INSERT INTO user (name, address, country) 
-          VALUES (?, ?,?)`,
-      [name, address, country]
-    );
-    res.status(202).json({
-      message: "User Created",
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: err,
-    });
-  }
-});
-
-app.get("/getusers", async (req, res) => {
-  try {
-    const data = await connection.promise().query(`SELECT *  from user;`);
-    res.status(200).json({
-      message: "Data retrieved successfully finally sayan again 4",
-      users: data[0],
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: err,
-    });
-  }
-});
-
 app.get("/about", async (req, res) => {
   try {
     res.status(200).json({
       message: "this is about page",
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: err,
-    });
-  }
-});
-
-app.get("/user/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const data = await connection
-      .promise()
-      .query(`SELECT *  from user where id = ?`, [id]);
-    res.status(200).json({
-      user: data[0][0],
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: err,
-    });
-  }
-});
-
-app.patch("/user/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { name, address, country } = req.body;
-    const update = await connection
-      .promise()
-      .query(
-        `UPDATE user set name = ?, address = ?, country = ? where id = ?`,
-        [name, address, country, id]
-      );
-    res.status(200).json({
-      message: "updated",
-    });
-  } catch (err) {
-    res.status(500).json({
-      message: err,
-    });
-  }
-});
-
-app.delete("/user/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const update = await connection
-      .promise()
-      .query(`DELETE FROM  user where id = ?`, [id]);
-    res.status(200).json({
-      message: "deleted",
     });
   } catch (err) {
     res.status(500).json({
